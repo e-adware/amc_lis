823759879f6015d11aeb04f3a69e763550671a72
Sourav Jyoti <sjyotitamuli@gmail.com>
2025-07-28 16:28:55 +0530

    hide inactive users in login screen

--- load_login_user.php
+++ load_login_user.php
@@ -1,28 +1,28 @@
 <?php
 include("includes/connection.php");
-$val=$_POST['val'];
+$val = $_POST['val'];
 
 $not_accountant = array();
 array_push($not_accountant, 0);
-$not_accountant = join(',',$not_accountant);
+$not_accountant = join(',', $not_accountant);
 
-$name=mysqli_query($GLOBALS["___mysqli_ston"], " SELECT `emp_id`,`name` FROM `employee` WHERE `name` like '%$val%' AND `levelid` NOT IN ($not_accountant) order by `name` ");
+$name = mysqli_query($GLOBALS["___mysqli_ston"], " SELECT `emp_id`,`name` FROM `employee` WHERE `name` like '%$val%' AND `status` = '0' AND `levelid` NOT IN ($not_accountant) order by `name` ");
 ?>
 <table class="table table-bordered user-table">
-	<th>ID</th><th>Name</th>
+	<th>ID</th>
+	<th>Name</th>
 	<?php
-	$i=1;
-	while($user=mysqli_fetch_array($name))
-	{
+	$i = 1;
+	while ($user = mysqli_fetch_array($name)) {
 		echo "<tr id='upd_psim$i' onclick=\"get_user_detail('@$user[name]@$user[emp_id]')\" style='cursor:pointer;'><td>$user[emp_id]</td><td>$user[name]";
-		?>	
-		<div id="log_det<?php echo $i;?>" style="display:none">
-			<?php echo "@".$user['name']."@".$user['emp_id'];?>	
+		?>
+		<div id="log_det<?php echo $i; ?>" style="display:none">
+			<?php echo "@" . $user['name'] . "@" . $user['emp_id']; ?>
 		</div>
 		<?php
 		echo "</td></tr>";
 		$i++;
 	}
-	
+
 	?>
 </table>
